c2991945724445da2e4588af2ef36126e7613e88
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2021-01-02 17:55:46 -0700

    Update double_dqn_cnn.py

--- double-dqn/double_dqn_cnn.py
+++ double-dqn/double_dqn_cnn.py
@@ -7,157 +7,201 @@ from collections import deque
 import tensorflow as tf
 import PIL as pil
 import time
+import gym_moving_dot
 
-
-class double_dqn_agent(object):
-    def __init__(self, action_size, batch_size):
+class DQN_NN(object):
+    def __init__(self, action_size):
         self.history = 4
         self.action_space = action_size
         self.q_network = self.make_net()
-        self.target = self.make_net()
+        self.q_target = self.make_net()
         self.move_weights()
-        self.memory = deque(maxlen=1000000)
-        self.batch = batch_size
-        # Q Learning Parameters
-        self.gamma = 0.95 # DISCOUNT FACTOR, CLOSE TO 1 = LONG TERM
-        self.epsilon = 1.0 # Exploration rate
-        self.epsilon_decay = 0.995
-        self.epsilon_min = 0.01
-        self.tau = 0.01
+        self.buff = 1000000
+        self.states = np.zeros((self.buff, 84, 84, 4), dtype=np.uint8)
+        self.actions = np.zeros((self.buff, 1))
+        self.rewards = np.zeros((self.buff, 1))
+        self.dones = np.zeros((self.buff, 1))
+        self.next_states = np.zeros((self.buff, 84, 84, 4), dtype=np.uint8)
+        self.counter = 0
+        self.batch = 32
+        self.gamma = 0.99 
+        self.epsilon = 1.0
+        self.epsilon_decay_frames = 1000000
+        self.epsilon_min = 0.02
+        self.learning_rate = 3e-4
+        self.opt = tf.keras.optimizers.Adam(lr=self.learning_rate)
+        self.tau = 0.001
+        self.iter = 0
+        self.training_frequency = 4
+        self.update = 10000
+
+    def move_weights(self):
+        self.q_target.set_weights(self.q_network.get_weights())
 
     def make_net(self):
+        init = tf.keras.initializers.VarianceScaling(scale=2)
         inputs = tf.keras.layers.Input(shape=(84,84,self.history))
-        x = tf.keras.layers.Conv2D(32, (8,8), strides=4, activation='relu', name='conv1')(inputs)
-        x = tf.keras.layers.Conv2D(64, (4,4), strides=2, activation='relu', name='conv2')(x)
-        x = tf.keras.layers.Conv2D(64, (3,3), strides=1, activation='relu', name='conv3')(x)
+        x = tf.keras.layers.Conv2D(32, 8, strides=(4,4), activation='relu', kernel_initializer=init)(inputs)
+        x = tf.keras.layers.Conv2D(64, 4, strides=(3,3), activation='relu', kernel_initializer=init)(x)
+        x = tf.keras.layers.Conv2D(64, 3, strides=(1,1), activation='relu', kernel_initializer=init)(x)
+        #x = tf.keras.layers.Conv2D(128, 3, strides=(1,1), activation='relu')(x)
         x = tf.keras.layers.Flatten()(x)
-        x = tf.keras.layers.Dense(512, activation='relu', name='dense1')(x)
+        x = tf.keras.layers.Dense(1024, activation='relu', kernel_initializer=init)(x)
+        x = tf.keras.layers.Dense(512, activation='relu', kernel_initializer=init)(x)
         x = tf.keras.layers.Dense(self.action_space, name='output')(x)
         model = tf.keras.models.Model(inputs=inputs, outputs=x)
-        model.compile(optimizer=tf.keras.optimizers.Adam(), loss="huber_loss", metrics=['mae'])
+        model.summary()
         return model
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def move_weights(self):
-        self.target.set_weights(self.q_network.get_weights())
-
-    def update_prime(self):
-        qweights = np.asarray(self.q_network.get_weights())
-        qprimeweights = np.asarray(self.target.get_weights())
-        qprimeweights = self.tau * qweights + (1 - self.tau) * qprimeweights
-        self.target.set_weights(qprimeweights)
+        i = self.counter % self.buff
+        self.states[i] = state
+        self.actions[i] = action
+        self.rewards[i] = reward
+        self.next_states[i] = next_state
+        self.dones[i] = int(done)
+        self.counter += 1
 
     def get_action(self, obs):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= 1/self.epsilon_decay_frames
         if random.random() < self.epsilon: 
             return np.random.choice(self.action_space)
         else:
-            obs = np.concatenate([obs[0], obs[1], obs[2], obs[3]], axis=2)
             obs = obs / 255.
-            return np.argmax(self.q_network.predict(np.array([obs,]))[0])
+            act = self.q_network(np.array([obs,])).numpy()[0]
+            act = np.argmax(act)
+            return act 
 
-    def train(self, iteration):
-        minibatch = random.sample(self.memory, self.batch)
-        states, targets = [], []
-        for state, action, reward, next_state, done in minibatch:
-            state = np.concatenate((state[0], state[1], state[2], state[3]), axis=2)
-            state = state / 255.
-            states.append(state)
-            state = np.array([state,])
-            next_state = np.concatenate((next_state[0], next_state[1], next_state[2], next_state[3]), axis=2)
-            next_state = next_state / 255.
-            next_state = np.array([next_state,])
-            self.q_network.predict(state)
-            target_f = self.q_network.predict(state)[0]
-            if done:
-                target_f[action] = reward
-            else:
-                q_pred = np.amax(self.target.predict(next_state)[0])
-                target_f[action] = reward + self.gamma * q_pred
-            targets.append(targets)
-            target_f = np.array([target_f,])
-            self.q_network.fit(state, target_f, epochs=1, verbose=0)
-        #self.q_network.fit(np.array(states), np.array(targets), batch_size=self.batch, epochs=1, verbose=1) # Batch training
-        
-        if iteration % 10 == 0:
-            self.move_weights()
+    @tf.function
+    def update_target(self, target_weights, weights, tau):
+        for (a, b) in zip(target_weights, weights):
+            a.assign(b * tau + a * (1 - tau))
+
+    #@tf.function
+    def train(self):
+        batch_indices = np.random.choice(min(self.counter, self.buff), self.batch)
+        state_batch = tf.convert_to_tensor(self.states[batch_indices] / 255.)
+        action_batch = tf.convert_to_tensor(self.actions[batch_indices])
+        action_batch = [[i, action_batch[i][0]] for i in range(len(action_batch))]
+        reward_batch = tf.convert_to_tensor(self.rewards[batch_indices])
+        dones_batch = tf.convert_to_tensor(self.dones[batch_indices])
+        reward_batch = tf.cast(reward_batch, dtype=tf.float32)
+        action_batch = tf.cast(action_batch, dtype=tf.int32)
+        dones_batch = tf.cast(dones_batch, dtype=tf.float32)
+        next_state_batch = tf.convert_to_tensor(self.next_states[batch_indices] /255.)
+        # Train critic
+        with tf.GradientTape() as tape:
+            next_q = self.q_target(next_state_batch)
+            y = reward_batch + (1 - dones_batch) * self.gamma * next_q
+            q = self.q_network(state_batch, training=True)
+            pred = tf.gather_nd(q, action_batch)
+            pred = tf.reshape(pred, [self.batch, 1])
+            msbe = tf.math.reduce_mean(tf.math.square(y - pred))
+        grads = tape.gradient(msbe, self.q_network.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.q_network.trainable_variables))
+
+        if self.iter % self.update == 0:
+            self.update_target(self.q_target.trainable_variables, self.q_network.trainable_variables, 1)
         else:
-            self.update_prime()
+            self.update_target(self.q_target.trainable_variables, self.q_network.trainable_variables, self.tau)
 
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+        self.iter += 1
 
 def preprocess(encode_frame):
     img = pil.Image.fromarray(encode_frame, 'RGB').convert('L').resize((84,110))
     img = img.crop((0, 18, 84, 102))
+    #img.show()
     img = np.asarray(img.getdata(), dtype=np.uint8).reshape(img.size[0], img.size[1], 1)
     return img
-    
+
+def framestack(obs):
+    return np.concatenate([obs[0], obs[1], obs[2], obs[3]], axis=2)
 
 # Hyperparameters
-ITERATIONS = 20000
+ITERATIONS = 10000
 batch_size = 32
-windows = 100
-learn_delay = 1000
+windows = 10
+learn_delay = 50000
 
-env = gym.make("Pong-v0").env
+#env = gym.make("Breakout-v4")
+env = gym.make("Pong-v4")
+#env = gym.make("MovingDotDiscrete-v0")
 
 print(env.action_space)
 print(env.observation_space, env.observation_space.shape)
-agent = double_dqn_agent(env.action_space.n, batch_size)
-rewards = []
-# Uncomment the line before to load model
-#agent.q_network = tf.keras.models.load_model("pong_ddqn.h5")
+agent = DQN_NN(env.action_space.n)
+fs = []
+agent.q_network = tf.keras.models.load_model("pong_nn.h5")
+agent.q_target = tf.keras.models.load_model("pong_nn.h5")
 avg_reward = deque(maxlen=ITERATIONS)
 best_avg_reward = -math.inf
 rs = deque(maxlen=windows)
 frames = 0
-for i in range(ITERATIONS):
+i = 0
+
+try:
+    avg_reward = deque(list(np.load("avg_reward.npy")), maxlen=ITERATIONS)
+except:
+    pass
+try:
+    fs = list(np.load("frames.npy"))
+    learn_delay = 0
+    frames = fs[-1]
+    agent.iter = frames
+except:
+    pass
+
+i = len(fs)
+agent.epsilon = 0.55
+while i < ITERATIONS:
     env.reset()
     s1, reward, done, info = env.step(env.action_space.sample())
-    s2 = preprocess(s1)
-    states = [s2, s2, s2, s2]
+    s1 = preprocess(s1)
+    states = [s1, s1, s1, s1]
     total_reward = 0
     j = 0
     while not done:
         #env.render()
-        action = agent.get_action(states)
+        action = agent.get_action(framestack(states))
         s2, reward, done, info = env.step(action)
         total_reward += reward
-        prev = states
+        prev = states.copy()
         states = states[1:]
         states.append(preprocess(s2))
-        agent.remember(prev, action, reward, states, done)
-        if len(agent.memory) > learn_delay and done:
-            agent.train(i)
-        if done:
-            rewards.append(total_reward)
-            rs.append(total_reward)
-        else:
-            s1 = s2
+        agent.remember(framestack(prev), action, reward, framestack(states), done)
+        if agent.counter > learn_delay and frames % agent.training_frequency == 0:
+            #agent.iter += 1
+            agent.train()
+        s1 = s2
         j += 1
         frames += 1
-        #print(frames)
+    rs.append(total_reward)
+    fs.append(frames)
+    avg = np.mean(rs)
+    avg_reward.append(avg)
     if i >= windows:
-        avg = np.mean(rs)
-        avg_reward.append(avg)
+        if i % 20 == 0:
+            agent.q_network.save("pong_nn.h5")
+            temp = np.array(avg_reward)
+            temp2 = np.array(fs)
+            np.save("frames", temp2)
+            np.save("avg_reward", temp)
         if avg > best_avg_reward:
             best_avg_reward = avg
-            agent.q_network.save("pong_ddqn.h5")
-    else: 
-        avg_reward.append(-21)
     
-    print("\rEpisode {}/{} || Best average reward {}, Current Iteration Reward {}, Frames {}".format(i, ITERATIONS, best_avg_reward, total_reward, frames))#, end='', flush=True)
-
-np.save("drewards", np.asarray(rewards))
-np.save("daverages", np.asarray(avg_reward))
-'''
-plt.ylim(0,200)
-plt.plot(rewards, color='olive', label='Reward')
+    print("\rEpisode {}/{} || Best average reward {}, Current Average {}, Current Iteration Reward {}, Frames {}, # Updates {}, Epislon {:.4f}".format(i, ITERATIONS, best_avg_reward, avg, total_reward, frames, agent.iter, agent.epsilon), end='', flush=True)
+    i += 1
+avg_reward = np.array(avg_reward)
+print(avg_reward.shape)
 plt.plot(avg_reward, color='red', label='Average')
 plt.legend()
 plt.ylabel('Reward')
-plt.xlabel('Generation')
+plt.xlabel('Iteration')
 plt.show()
-'''
+
+plt.plot(fs, avg_reward, color='red', label='Average')
+plt.legend()
+plt.ylabel('Reward')
+plt.xlabel('Frames')
+plt.show()
