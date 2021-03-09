7b343fe22cb75f9517430278207accdda86fc8b9
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2021-03-09 14:16:20 -0700

    Update td3.py

--- td3/td3.py
+++ td3/td3.py
@@ -5,70 +5,69 @@ import matplotlib.pyplot as plt
 import math
 import tensorflow as tf
 from collections import deque
+import pybulletgym  # register PyBullet enviroments with open ai gym
 
-#tf.compat.v1.disable_eager_execution()
-#tf.keras.backend.set_floatx('float64')
-class DDPG_AGENT(object):
-    def __init__(self, action_size, state_size, batch_size):
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+class TD3(object):
+    def __init__(self, action_size, state_size):
         self.action_space = action_size
         self.state_space = state_size
-        self.act_range = 2.0
-        self.actor_opt = tf.keras.optimizers.Adam(lr=0.001)
-        self.critic_opt = tf.keras.optimizers.Adam(lr=0.001)
+        self.act_range = 1.0
+        self.actor_opt = tf.keras.optimizers.Adam(lr=1e-3)
+        self.critic_opt = tf.keras.optimizers.Adam(lr=1e-3)
         self.q1 = self.make_critic()
         self.q2 = self.make_critic()
         self.q1_target = self.make_critic()
         self.q2_target = self.make_critic()
         self.policy = self.make_actor()
         self.policy_target = self.make_actor()
+        #self.policy.summary()
+        #self.q1.summary()
         self.policy_counter = 0
         self.move_weights()
-        self.buff = 50000
+        self.buff = int(1e6)
         self.states = np.zeros((self.buff, self.state_space[0]))
         self.actions = np.zeros((self.buff, self.action_space[0]))
         self.rewards = np.zeros((self.buff, 1))
+        self.dones = np.zeros((self.buff, 1))
         self.next_states = np.zeros((self.buff, self.state_space[0]))
         self.counter = 0
-        self.batch = batch_size
-        self.gamma = 0.99 # DISCOUNT FACTOR, CLOSE TO 1 = LONG TERM
+        self.batch = 100
+        self.gamma = 0.99 
         self.tau = 0.005
-        self.policy_delay = 3
-        # Noise variables
-        self.mean = 0
-        self.std = 0.1
+        self.act_noise = 0.1 * self.act_range
+        self.target_noise = 0.2 * self.act_range
+        self.noise_clip = 0.5
+        self.policy_delay = 2
      
     def make_critic(self):
         state_ = tf.keras.layers.Input(shape=(self.state_space[0]))
         action_ = tf.keras.layers.Input(shape=(self.action_space[0]))
-        state = tf.keras.layers.Dense(32, activation='relu', name='state1')(state_)
-        #state = tf.keras.layers.Dense(32, activation='relu', name='state2')(state)
-        action = tf.keras.layers.Dense(32, activation='relu', name='act1')(action_)
-        x = tf.keras.layers.Concatenate()([state, action])
-        x = tf.keras.layers.Dense(256, activation='relu', name='dense2')(x)
-        x = tf.keras.layers.Dense(256, activation='relu', name='dense3')(x)
+        x = tf.keras.layers.Concatenate()([state_, action_])
+        x = tf.keras.layers.Dense(400, activation='relu')(x)
+        x = tf.keras.layers.Dense(300, activation='relu')(x)
         x = tf.keras.layers.Dense(self.action_space[0], name='output')(x)
         model = tf.keras.models.Model(inputs=[state_, action_], outputs=x)
-        model.summary()
         return model
     
     def make_actor(self):
         last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
         state_ = tf.keras.layers.Input(shape=(self.state_space[0]))
-        x = tf.keras.layers.Dense(128, activation='relu', name='dense1')(state_)
-        x = tf.keras.layers.Dense(256, activation='relu', name='dense2')(x)
-        x = tf.keras.layers.Dense(128, activation='relu', name='dense3')(x)
+        x = tf.keras.layers.Dense(400, activation='relu')(state_)
+        x = tf.keras.layers.Dense(300, activation='relu')(x)
         x = tf.keras.layers.Dense(self.action_space[0], activation='tanh', name='output', kernel_initializer=last_init)(x)
         x = x * self.act_range
         model = tf.keras.models.Model(inputs=state_, outputs=x)
-        model.summary()
         return model
 
-    def remember(self, state, action, reward, next_state, _):
+    def remember(self, state, action, reward, next_state, done):
         i = self.counter % self.buff
         self.states[i] = state
         self.actions[i] = action
         self.rewards[i] = reward
         self.next_states[i] = next_state
+        self.dones[i] = int(done)
         self.counter += 1
 
     def move_weights(self):
@@ -77,10 +76,12 @@ class DDPG_AGENT(object):
         self.policy_target.set_weights(self.policy.get_weights())
 
     def get_action(self, obs):
-        action = tf.squeeze(self.policy(np.array([obs,])))
-        act = action.numpy() + np.random.normal(self.mean, self.std, 1)[0]
+        action = tf.squeeze(self.policy(np.array([obs])))
+        act = action.numpy() + np.clip(np.random.normal(0, self.act_noise, 1)[0], -self.noise_clip, self.noise_clip)
         act = np.clip(act, -self.act_range, self.act_range)
-        return [np.squeeze(act)]
+        if isinstance(act, float):
+            return [act]
+        return act
 
     def train(self):
         batch_indices = np.random.choice(min(self.counter, self.buff), self.batch)
@@ -89,12 +90,14 @@ class DDPG_AGENT(object):
         reward_batch = tf.convert_to_tensor(self.rewards[batch_indices])
         reward_batch = tf.cast(reward_batch, dtype=tf.float32)
         next_state_batch = tf.convert_to_tensor(self.next_states[batch_indices])
-        # Train critic
+        dones_batch = tf.convert_to_tensor(self.dones[batch_indices])
+        dones_batch = tf.cast(dones_batch, dtype=tf.float32)
+        
         with tf.GradientTape(persistent=True) as tape:
-            targets_act = tf.clip_by_value(tf.math.add(self.policy_target(next_state_batch, training=True),  tf.random.normal([self.batch, 1], self.mean, self.std)), -self.act_range, self.act_range)
-            bellman = reward_batch + self.gamma * tf.math.minimum(self.q1_target([next_state_batch, targets_act], training=True), self.q2_target([next_state_batch, targets_act], training=True))
-            critic1 = self.q1([state_batch, action_batch], training=True)
-            critic2 = self.q2([state_batch, action_batch], training=True)
+            targets_act = tf.clip_by_value(self.policy_target(next_state_batch) + tf.random.normal([self.batch, 1], 0, self.target_noise), -self.noise_clip, self.noise_clip)
+            bellman = reward_batch + (1 - dones_batch) * self.gamma * tf.math.minimum(self.q1_target([next_state_batch, targets_act]), self.q2_target([next_state_batch, targets_act]))
+            critic1 = self.q1([state_batch, action_batch])
+            critic2 = self.q2([state_batch, action_batch])
             msbe1 = tf.math.reduce_mean(tf.math.square(bellman - critic1))
             msbe2 = tf.math.reduce_mean(tf.math.square(bellman - critic2))
         
@@ -109,12 +112,12 @@ class DDPG_AGENT(object):
         if self.policy_counter % self.policy_delay == 0:
             with tf.GradientTape() as tape:
                 actions = self.policy(state_batch, training=True)
-                critic = tf.math.minimum(self.q1([state_batch, actions], training=True), self.q2([state_batch, actions], training=True))
+                #critic = tf.math.minimum(self.q1([state_batch, actions]), self.q2([state_batch, actions]))
+                critic = self.q1([state_batch, actions])
                 policy_loss = -tf.math.reduce_mean(critic)
             
             policy_gradients = tape.gradient(policy_loss, self.policy.trainable_variables)
             self.actor_opt.apply_gradients(zip(policy_gradients, self.policy.trainable_variables))
-
 
             self.update_target(self.policy_target.trainable_variables, self.policy.trainable_variables)
        
@@ -125,59 +128,60 @@ class DDPG_AGENT(object):
         for (a, b) in zip(target_weights, weights):
             a.assign(b * self.tau + a * (1 - self.tau))
 
-# Hyperparameters
-ITERATIONS = 120
-batch_size = 100
-windows = 20
-learn_delay = 200
+if __name__ == "__main__":
+    # Hyperparameters
+    steps = int(2e5)
+    windows = 50
+    learn_delay = int(1e4)
 
-env = gym.make("Pendulum-v0")
-'''env.observation_space.shape'''
-print(env.action_space, env.action_space.shape)
-print(env.observation_space, env.observation_space.shape)
-agent = DDPG_AGENT(env.action_space.shape, env.observation_space.shape, batch_size)
-rewards = []
-avg_reward = deque(maxlen=ITERATIONS)
-best_avg_reward = -math.inf
-rs = deque(maxlen=windows)
-
-for i in range(ITERATIONS):
-    s1 = env.reset()
-    total_reward = 0
-    done = False
+    env = gym.make("HalfCheetahPyBulletEnv-v0")
+    #env = gym.make("HumanoidFlagrunPyBulletEnv-v0")
+    '''env.observation_space.shape'''
+    print(env.action_space, env.action_space.shape)
+    print(env.observation_space, env.observation_space.shape)
+    minn = -1
+    maxx = 1
+    agent = TD3(env.action_space.shape, env.observation_space.shape)
+    rewards = []
+    avg_reward = deque(maxlen=steps)
+    best_avg_reward = -math.inf
+    rs = deque(maxlen=windows)
+    i = 0
     step = 0
-    while not done:
-        #env.render()
-        action = agent.get_action(s1)
-        s2, reward, done, info = env.step(action)
-        total_reward += reward
-        agent.remember(s1, action, reward, s2, done)
-        if agent.counter > learn_delay:
-            agent.train()
-        if done:
-            rewards.append(total_reward)
-            rs.append(total_reward)
-        else:
+    while True:
+        s1 = env.reset()
+        total_reward = 0
+        done = False
+        while not done:
+            #env.render()
+            if step < learn_delay:
+                action = (maxx - minn) * np.random.random(env.action_space.shape) + minn
+            else:
+                action = agent.get_action(s1)
+            s2, reward, done, info = env.step(action)
+            total_reward += reward
+            agent.remember(s1, action, reward, s2, done)
+            if agent.counter > learn_delay:
+                agent.train()
             s1 = s2
-        step += 1
-    if i >= windows:
+            step += 1
+        rs.append(total_reward)
         avg = np.mean(rs)
         avg_reward.append(avg)
         if avg > best_avg_reward:
             best_avg_reward = avg
-            #agent.q_network.save("dqn_cartpole.h5")
-    else: 
-        avg_reward.append(-2000)
-    
-    print("\rEpisode {}/{} || Best average reward {}, Current Iteration Reward {}".format(i, ITERATIONS, best_avg_reward, total_reward) , end='', flush=True)
+        
+        print("\rStep {}/{} Iteration {} || Best average reward {}, Current Average {}, Current Iteration Reward {}".format(step, steps, i, best_avg_reward, avg, total_reward), end='', flush=True)
+        i += 1
+        if step >= steps:
+            break
 
-#np.save("rewards", np.asarray(rewards))
-#np.save("averages", np.asarray(avg_reward))
-#plt.ylim(-2000,1)
-plt.plot(rewards, color='olive', label='Reward')
-plt.plot(avg_reward, color='red', label='Average')
-plt.legend()
-plt.title("TD3 On Pendulum")
-plt.ylabel('Reward')
-plt.xlabel('Generation')
-plt.show()
+    #np.save("rewards", np.asarray(rewards))
+    #np.save("tf_td3_ant_0", np.asarray(avg_reward))
+    plt.plot(rewards, color='olive', label='Reward')
+    plt.plot(avg_reward, color='red', label='Average')
+    plt.legend()
+    plt.title("Lunar Lander")
+    plt.ylabel('Reward')
+    plt.xlabel('Step')
+    plt.show()
